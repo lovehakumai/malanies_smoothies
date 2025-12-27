4079ad0080c745ef8443af95e4e0efd68acfdf95
Masaharu Ura (Pogi Masa) <50307956+lovehakumai@users.noreply.github.com>
2025-12-27 17:10:31 +0900

    Fetch smoothie data and display in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,9 @@ if ingredients_list:
 
     for fruite_chosen in ingredients_list:
         ingredients_string += fruite_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
     st.write(ingredients_string)
 
@@ -41,7 +44,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response)
-st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
