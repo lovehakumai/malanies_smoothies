02b596e9f067ff1ba367112fbcabb5c3ed4a2579
Masaharu Ura (Pogi Masa) <50307956+lovehakumai@users.noreply.github.com>
2025-12-27 17:13:29 +0900

    Fix typo in variable name for fruit processing

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,9 +26,9 @@ ingredients_list = st.multiselect(
 if ingredients_list:
     ingredients_string = ''
 
-    for fruite_chosen in ingredients_list:
+    for fruit_chosen in ingredients_list:
         ingredients_string += fruite_chosen + ' '
-        st.subheader(fruite_chosen + ' Nutrition Information')
+        st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
